1cffa28ffc8ad56f77f10b75c4440aa248a1969b
ngoctinvo <ngoctinvo2001@gmail.com>
2022-05-04 21:30:09 +0700

    toggle theme mode -chuaxong

--- js/scroll_to_top.js
+++ js/scroll_to_top.js
@@ -10,3 +10,23 @@ $('#backToTop').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '100');
 });
+
+const toggleSwitch = document.querySelector('.sidebarMini input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+toggleSwitch.addEventListener('change', switchTheme);
