7e9441255a53129c94390ae64fe5e3cc9a9c205a
ngoctinvo <ngoctinvo2001@gmail.com>
2022-05-05 16:31:01 +0700

    dark mode for home-about-service

--- js/scroll_to_top.js
+++ js/scroll_to_top.js
@@ -14,7 +14,7 @@ $('#backToTop').on('click', function(e) {
 const toggleSwitch = document.querySelector('.sidebarMini input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    document.documentElement.setAttribute('theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
@@ -22,10 +22,10 @@ if (currentTheme) {
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('theme', 'dark');
         localStorage.setItem('theme', 'dark');
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('theme', 'light');
         localStorage.setItem('theme', 'light');
     }
 }
